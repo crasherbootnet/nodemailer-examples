94b6b0def0a4a77652955ee6a06b978269de7b34
KOUAME DAVID <kouamedavid124@gmail.com>
2020-05-17 09:57:05 +0000

    sent email with queue

--- sendTemplates/server.js
+++ sendTemplates/server.js
@@ -3,7 +3,9 @@
     Don't forget to disable less secure app from Gmail: https://myaccount.google.com/lesssecureapps TODO:
 */
 
-require('dotenv').config();
+///////////////////////////////////////////////////////////////
+// Send email
+// require('dotenv').config();
 
 const nodemailer = require('nodemailer');
 const hbs = require('nodemailer-handlebars');
@@ -13,8 +15,9 @@ const log = console.log;
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.EMAIL || 'abc@gmail.com', // TODO: your gmail account 
-        pass: process.env.PASSWORD || '1234' // TODO: your gmail password
+        // user: process.env.EMAIL || 'bootnetcrasher@gmail.com', // TODO: your gmail account 
+        user: 'bootnetcrasher@gmail.com', // TODO: your gmail account 
+        pass: '29121990marie' // TODO: your gmail password
     }
 });
 
@@ -23,12 +26,11 @@ transporter.use('compile', hbs({
     viewEngine: 'express-handlebars',
     viewPath: './views/'
 }));
-
-
+/*
 // Step 3
 let mailOptions = {
-    from: 'tabbnabbers@gmail.com', // TODO: email sender
-    to: 'deltamavericks@gmail.com', // TODO: email receiver
+    from: 'bootnetcrasher@gmail.com',
+    to: 'bootnetcrasher@gmail.com',
     subject: 'Nodemailer - Test',
     text: 'Wooohooo it works!!',
     template: 'index',
@@ -44,3 +46,72 @@ transporter.sendMail(mailOptions, (err, data) => {
     }
     return log('Email sent!!!');
 });
+*/
+///////////////////////////////////////////////////////////////
+// Queue
+/*const queue = require('queue');
+var q = queue();
+var results = [];
+// add jobs using the familiar Array API
+q.push(function (cb) {
+    results.push('two')
+    cb()
+  })*/
+
+  var Queue = require('better-queue');
+
+/*var q = new Queue(function (input, cb) {
+  
+  // Some processing here ...
+  console.log("execute job");
+  cb(null, result);
+})
+q.push(1)
+q.push({ x: 1 })*/
+
+/*function checker (data, callback) {
+  console.log(data)
+  callback()
+}
+
+const q = new Queue(checker)
+
+function test () {
+  q.push({data: 1})
+  q.push({data: 2})
+  q.push({data: 3})
+  q.push({data: 4})
+}
+
+test()*/
+
+var q = new Queue(function (input, cb) {
+    let mailOptions = {
+        from: 'bootnetcrasher@gmail.com',
+        // to: 'bootnetcrasher@gmail.com',
+        to: input,
+        subject: 'Nodemailer - Test',
+        text: 'Wooohooo it works!!',
+        template: 'index',
+        context: {
+            name: 'Accime Esterling'
+        } // send extra values to template
+    };
+
+    transporter.sendMail(mailOptions, (err, data) => {
+        if (err) {
+            return log('Error occurs');
+        }
+        // return log('Email sent!!!');
+    });
+  cb(null, result);
+});
+q.push("bootnetcrasher@gmail.com", function (err, result) {
+    // console.log("execution de la tache");
+});
+q.push("bootnetcrasher@gmail.com", function (err, result) {
+    // console.log("execution de la tache");
+});
+
+
+console.log("Sent mail !");
